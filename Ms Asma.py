5c9c16473928710268966dded68aa40c91945183
AsmaAsif12 <besttrack28@gmail.com>
2026-01-20 23:04:34 +0500

    Add files via upload

---
+++ Ms Asma.py
@@ -0,0 +1,48 @@
+#Find area of circle by using formula in python :
+
+radius = float ( input ("Enter the radius : "))
+area = math.pi * radius * * 2
+print("Area of the circle :" ,area)
+Enter the radius of a circle is : 3
+
+Area of a circle is : 28.25999999
+
+
+
+
+
+
+
+
+# find Area of triangle by using formula in python :
+
+Area of triangle : 0.5 * base * height
+height =float (input ("Enter the height of the triangle : "))
+base = float ( input ( " Enter the base of the triangle : "))
+area = ( 0.5) * base * height
+print ( the area of triangle is " , area")
+the height of the triangle:5
+The base of the triangle :7
+
+Area of triangle is 17.5
+
+
+
+
+
+# find area of square by using formula in python :
+
+Formula : Area = side * side
+def  area square ( side):
+return side  * * 2
+# Example
+print ( area of square (5))
+# Output : 25
+
+Using user input
+
+side = float ( input(Enter the side length of the square : " ))
+area = side * * 2
+print ( " Area of square :" , area)
+# output : 25
+
